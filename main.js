d6962c61ba77f9c611b30eb90ee5fc2cc7bd2494
ohmyguigs <ohmyguigs@gmail.com>
2020-08-23 21:06:54 -0300

    prevents being stuck for too long

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 // magic consts
 const MIN_BOOST_DIST = 2000;
 const MIN_BOOST_ANGLE = 1;
-const MIN_BOOST_CHECKPOINTS = 3;
+const MIN_BOOST_CHECKPOINTS = 0;
+const EDGE_ANGLE = 82;
+const MAX_BREAKING_ROUNDS = 3;
 
 const BREAK_DIST_1 = 1300;
+const BREAK_SPEED_1 = 69;
 const BREAK_DIST_2 = 1100;
+const BREAK_SPEED_2 = 42;
 const BREAK_DIST_3 = 800;
+const BREAK_SPEED_3 = 13;
 
 // AUX vars
 let boostAvailable = true;
-let loopCount = 0;
 let checkpoint = 0;
 let lastCPdist = 0;
 let lastChk = 0;
+let breakCount = 0;
 
 // AUX funcs
 const getRealDist = (px, py, gx, gy) => Math.sqrt((gx - px) * (gx - px) + (gy - py) * (gy - py));
+const hitTheBreaks = (speed) => {
+  if (breakCount >= MAX_BREAKING_ROUNDS) {
+    breakCount = 0;
+    return 100;
+  } else {
+    breakCount += 1;
+    return speed;
+  }
+}
 const getThrust = (dist, angle) => {
-  if (angle > 90) {
+  if (angle > EDGE_ANGLE) {
     return 0;
   } else if (
-    dist > MIN_BOOST_DIST &&
+    dist >= MIN_BOOST_DIST &&
     boostAvailable &&
     angle <= MIN_BOOST_ANGLE &&
     checkpoint >= MIN_BOOST_CHECKPOINTS
@@ -28,11 +42,11 @@ const getThrust = (dist, angle) => {
     boostAvailable = false;
     return 'BOOST';
   } else if (dist <= BREAK_DIST_3) {
-    return 25;
+    return hitTheBreaks(BREAK_SPEED_3);
   } else if (dist <= BREAK_DIST_2) {
-    return 50;
+    return hitTheBreaks(BREAK_SPEED_2);
   } else if (dist <= BREAK_DIST_1) {
-    return 75;
+    return hitTheBreaks(BREAK_SPEED_1);
   }
 
   return 100;
@@ -41,7 +55,6 @@ const getThrust = (dist, angle) => {
 
 // game loop
 while (true) {
-    loopCount += 1;
     var inputs = readline().split(' ');
     const x = parseInt(inputs[0]);
     const y = parseInt(inputs[1]);
@@ -76,13 +89,12 @@ while (true) {
 
     // dis just a log
     console.error({
-      loopCount,
-      nextCheckpointDist,
-      realDistance,
-      nextCheckpointAngle,
-      thrust,
-      boostAvailable,
-      checkpoint,
+      THRUST: thrust,
+      DIST: nextCheckpointDist,
+      REAL_DIST: realDistance,
+      ANGLE: nextCheckpointAngle,
+      HAS_BOOST: boostAvailable,
+      CHECKPOINT: checkpoint,
     })
 
     const command = `${
